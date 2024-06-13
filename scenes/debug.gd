fb50b94f46965e75584f975b1d5f21c1f3d2979c
0xAquaWolf <0xaquawolf@protonmail.com>
2024-06-13 21:06:44 +0700

    updates

--- scenes/debug.gd
+++ scenes/debug.gd
@@ -7,12 +7,13 @@ var fps : String
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	visible = false
-	add_debug_property("fps", "fps")
-	
+	add_debug_property("FPS", fps)
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	fps = "%.2f" % (1.0/delta)
-	property.text = property.name + ": " + str(fps)
+	if visible:
+		fps = "%.2f" % (1.0/delta)
+		property.text = property.name + ": " + str(fps)
 
 func _input(event):
 	if event.is_action_pressed("debug"):
